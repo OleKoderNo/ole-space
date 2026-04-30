53f4a34b55213b3dfecdffb2cec829e4f7510e5d
Ole <oleh.kodehode@gmail.com>
2026-05-01 01:38:44 +0200

    feat: hide search for logged out users

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,18 @@
-import { loadPublicFeed } from "./feed.js";
 import { renderNavbar } from "./navbar.js";
+import { loadPublicFeed } from "./feed.js";
 import { setupPostSearch } from "./search.js";
+import { isLoggedIn } from "./auth.js";
 
 const feedContainer = document.querySelector("#feed");
+const searchSection = document.querySelector("#search-section");
 
 renderNavbar();
 loadPublicFeed(feedContainer);
-setupPostSearch(feedContainer);
+
+if (isLoggedIn()) {
+	setupPostSearch(feedContainer);
+} else {
+	if (searchSection) {
+		searchSection.remove();
+	}
+}
