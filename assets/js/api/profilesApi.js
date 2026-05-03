1445873d341d8f49101f02b85387801533123420
Ole <oleh.kodehode@gmail.com>
2026-05-03 22:17:35 +0200

    docs: add JSDoc for profile API functions

--- assets/js/api/profilesApi.js
+++ assets/js/api/profilesApi.js
@@ -1,5 +1,11 @@
 import { API_BASE, getAuthHeaders, getJson } from "./api.js";
 
+/**
+ * Get profile username, post, follower and following count
+ *
+ * @param {*} name profile username
+ * @returns {Promise<object>} profile data
+ */
 export async function getProfileByName(name) {
 	const response = await fetch(
 		`${API_BASE}/social/profiles/${name}?_posts=true&_followers=true&_following=true`,
@@ -11,6 +17,12 @@ export async function getProfileByName(name) {
 	return getJson(response, "Could not fetch profile");
 }
 
+/**
+ * Follows another user
+ *
+ * @param {*} name username of profile to follow
+ * @returns {Promise<object>} updated profile data
+ */
 export async function followProfile(name) {
 	const response = await fetch(`${API_BASE}/social/profiles/${name}/follow`, {
 		method: "PUT",
@@ -20,6 +32,12 @@ export async function followProfile(name) {
 	return getJson(response, "Could not follow profile");
 }
 
+/**
+ * Unfollow another users profile
+ *
+ * @param {*} name username of profile to unfollow
+ * @returns {Promise<object>} update profile data
+ */
 export async function unfollowProfile(name) {
 	const response = await fetch(`${API_BASE}/social/profiles/${name}/unfollow`, {
 		method: "PUT",
